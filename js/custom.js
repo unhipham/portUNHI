68593c76b4b69562e4ab5ec5e2568d11eb7f12a0
unhipham <nhi.pnu2396@arena-multimedia.edu.vn>
2024-11-16 00:47:30 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+var typed = new Typed('.element', {
+               	strings: ["Web Designer.", "UI-UX Designer.","Apps Designer." ,"HTML- CSS."],
+       	 	typeSpeed:50,
+        		backSpeed:50,
+        		loop:true,
+        		});
+
+  new WOW().init();
